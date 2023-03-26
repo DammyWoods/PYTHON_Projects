fff6bd9a08f10e3e7d96cf07804157eb1b73618a
D.Obimuyiwa <damilolaobimuyiwa@gmail.com>
2023-03-27 00:17:34 +0100

    This program allows a user to access two different financial calculator, an investment calculator and a bond repayment calculator at Orange Bank.

--- finance_calculators.py
+++ finance_calculators.py
@@ -24,10 +24,10 @@ if choice == "investment":
     #Allows user to choose the kind of interest the investment will be calculated with
     if interest == "simple":
         interest_sim = investment_amount * (1 + (interest_rate/100)* investment_years)
-        print(f"\nDear {name}!,\nYour investment of £{investment_amount} will be worth £{interest_sim:.2f} at {interest_rate}% after {investment_years} years of {interest} interest.")
+        print(f"\nDear {name}!,\nYour investment of £{investment_amount:,.2f} will be worth £{interest_sim:,.2f} at {interest_rate}% after {investment_years} years of {interest} interest.")
     elif interest == "compound":
         interest_com = investment_amount * pow((1 + interest_rate/100), investment_years)
-        print(f"\nDear {name}!,\nYour investment of £{investment_amount} will be  n  worth £{interest_com:.1f} at {interest_rate}% after {investment_years} year(s) of {interest} interest.")
+        print(f"\nDear {name}!,\nYour investment of £{investment_amount:,.2f} will be worth £{interest_com:,.2f} at {interest_rate}% after {investment_years} year(s) of {interest} interest.")
     else:
         print("You have entered an invalid interest type! Please enter 'simple' or 'compound' to proceed.")
 
@@ -37,7 +37,7 @@ elif choice == 'bond':
     bond_interest_rate = float(input("Enter the interest rate for bond: "))
     bond_months = int(input("Enter the number of months you plan to take to repay the bond: "))
     monthly_repayment = (((bond_interest_rate/100) / 12) * home_present_value) / (1- (1 + ((bond_interest_rate/100) / 12)) ** (- bond_months))
-    print(f"\nDear {name}!,\nThe monthly repayment for your home loan which is currently valued at £{home_present_value} is £{monthly_repayment:.1f} at {bond_interest_rate}% for the next {bond_months}months")
+    print(f"\nDear {name}!,\nThe monthly repayment for your home loan which is currently valued at £{home_present_value:,.2f} is £{monthly_repayment:,.2f} at {bond_interest_rate}% for the next {bond_months}months")
 
 else: 
      print("You have entered an invalid choice of calculator! Please enter 'investment' or 'bond' to proceed.")   
